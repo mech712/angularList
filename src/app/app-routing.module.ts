470f3d310cc3a1d7ced9494e766fb46d951744ca
unknown <you@example.com>
2019-12-03 20:22:06 +0500

    роутинг

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './component/list/list.component';
+import { DocumentComponent } from './component/document/document.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:ListComponent},
+  {path:"list", component:ListComponent},
+  {path:"document", component:DocumentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
