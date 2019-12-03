50c898e06e1a4e862ea7b1e986ffc462d478ba7e
unknown <you@example.com>
2019-12-03 21:44:32 +0500

    компонент списка документов

--- src/app/component/list/list.component.ts
+++ src/app/component/list/list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DocumentService } from 'src/app/service/document.service';
+import { MyDocument } from 'src/app/model/document';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  docList:MyDocument[] = [];
 
-  constructor() { }
+  constructor(private docService:DocumentService) { }
 
   ngOnInit() {
+    this.docService.getList().subscribe(
+      (docs:MyDocument[]) => this.docList = docs
+    );
   }
 
 }
